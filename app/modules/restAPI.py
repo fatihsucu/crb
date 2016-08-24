c542e98d82e3b32bfcfcc5f52a91182dc2459018
fatihsucu <fatihsucu0@gmail.com>
2016-08-24 19:53:29 +0300

    fixes 404

--- app/modules/restAPI.py
+++ app/modules/restAPI.py
@@ -74,7 +74,7 @@ class RestAPI(object):
                 elem["title"] = r["name"]
                 elem["buttons"] = [{
                     "type": "web_url",
-                    "url": self.config.REPICE_PATH + r["url"] + "&utm_source=rse&utm_medium=facebook%20bot&utm_campaign=recipe%20tool",
+                    "url": self.config.REPICE_PATH + r["url"] + "?&utm_source=rse&utm_medium=facebook%20bot&utm_campaign=recipe%20tool",
                     "title": "Show Recipe"
                 },
                 {
